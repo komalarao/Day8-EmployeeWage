204ed20b1d68473351b9236f9ded64ba4c2a8701
komalarao <manoharvakamullu1999@gmail.com>
2023-01-18 10:28:31 -0800

    refactor: program to caluculate daily employeewage

--- src/com/bridgelabz/employeeWage/EmployeeWage.java
+++ src/com/bridgelabz/employeeWage/EmployeeWage.java
@@ -1,19 +1,26 @@
 package com.bridgelabz.employeeWage;
 import java.util.Random;
 public class EmployeeWage {
-	public static void main(String[] args) {
+	
+		public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation Program");
+			System.out.println("Welcome to Employee Wage Computation Program");
 
-		Random random = new Random();
-		
-		int randomNum = random.nextInt(2);
+			Random random = new Random(); 
+			int wagePerHour = 20;
+			int fullDayHours = 8;
+			int salary = 0;
 
-		// check attendance
-		if (randomNum == 1)
-			System.out.println("Employee  is Present");
-		else
-			System.out.println("Employee  is Absent");
+			
+			int randomNum = random.nextInt(2);
 
-	}
+			//check attendance
+			if (randomNum == 1) {
+				System.out.println("Employee  is Present");
+				salary = wagePerHour * fullDayHours;
+			} else
+				System.out.println("Employee  is Absent");
+			System.out.println("salary is: " + salary);
+
+		}
 }
